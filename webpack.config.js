c12d9b32256c99e263cc25c8c2d7106a93f65ce5
Kate <54095868+romcath@users.noreply.github.com>
2020-05-02 21:45:27 +0300

    Configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+/* eslint-disable global-require */
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/scripts/index.js',
+    article: './src/pages/article/article.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]/[name].[chunkhash].js',
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: { loader: 'babel-loader' },
+    },
+    {
+      test: /\.(eot|ttf|woff|woff2)$/,
+      loader: 'file-loader?name=./vendor/[name].[ext]',
+    },
+    {
+      test: /\.(png|jpg|gif|ico|svg)$/,
+      use: [
+        'file-loader?name=./images/[name].[ext]',
+        {
+          loader: 'image-webpack-loader',
+          options: {},
+        },
+      ],
+    },
+    {
+      test: /\.css$/i,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+    },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '[name]/[name].[contenthash].css' }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default'],
+      },
+      canPrint: true,
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: './src/pages/article/article.html',
+      filename: 'article/article.html',
+    }),
+    new WebpackMd5Hash(),
+  ],
+};
